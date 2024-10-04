6cded84e8ee855ec43ced02558fe570f7e18639d
Anudeep Yegireddi <anudeep.yegireddi@gmail.com>
2024-10-04 15:23:44 +0530

    chore: added a home and a home/create page

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,5 +1,24 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 const Home = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+  return (
+    <div className="bg-red-200 w-full items-center justify-center flex ">
+      <Button
+        onClick={() => {
+          router.push("home/create");
+        }}
+        variant="secondary"
+        className="gap-2 p-8 text-lg"
+      >
+        <PlusCircleIcon size={36} className="m-0" />
+        Create an Interviewer
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
