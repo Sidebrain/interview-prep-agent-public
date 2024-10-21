e86e849e7ba63e500483e86f3c72d21bb231559d
Anudeep Yegireddi <anudeep.yegireddi@gmail.com>
2024-10-21 11:22:04 +0530

    feat: updated the voice endpoint to output text response
    Notes:
    - Simple change that moved from verbose json to a text response

---
+++ backend/app/api/v1/endpoints/audio_router.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
+from app.services.llms.openai_client import openai_async_client
+
+router = APIRouter()
+
+
+@router.post("/transcribe", tags=["transcribe", "openai"])
+async def transcribe_audio(file: UploadFile = File(...)) -> JSONResponse:
+    print("Transcribing audio file")
+    print(file.filename, file.content_type)
+    try:
+
+        response = await openai_async_client.audio.transcriptions.create(
+            model="whisper-1",
+            file=(file.filename, file.file, file.content_type),
+            language="en",
+            # response_format="verbose_json",
+            response_format="text",
+            temperature=0.5,
+            timestamp_granularities=["segment"],
+            prompt="Transcribe the following audio file",
+            timeout=600,
+        )
+
+        print(response, end=f"\n{'-'*50}\n")
+        # return JSONResponse(content=response.model_dump_json())
+        return JSONResponse(content=response)
+    except Exception as e:
+        print("Failed with exception", e)
+        raise HTTPException(status_code=500, detail=str(e))
