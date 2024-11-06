0675038fc69234567380c8a4f01668763243e907
Anudeep Yegireddi <anudeep.yegireddi@gmail.com>
2024-11-06 14:44:14 +0530

    feat: added a button that allows you to use suggestion

--- frontend/app/home/create/components/Suggestion.tsx
+++ frontend/app/home/create/components/Suggestion.tsx
@@ -1,20 +1,51 @@
+import { Button } from "@/components/ui/button";
+import InputContext from "@/context/InputContext";
 import { frameRenderHandler } from "@/handlers/frameRenderHandler";
 import { FrameType } from "@/reducers/messageFrameReducer";
 import { Thought, ThoughtSchema } from "@/types/ScalableWebsocketTypes";
+import { CheckCircleIcon } from "lucide-react";
+import { useContext, useRef } from "react";
 
 type SuggestionProps = {
   frame: FrameType;
 };
 
 const Suggestion = ({ frame }: SuggestionProps) => {
+  const { dispatch: dispatchInputValue } = useContext(InputContext);
+  const suggestionRef = useRef<HTMLParagraphElement>(null);
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    dispatchInputValue({
+      type: "SET_INPUT",
+      payload: suggestionRef.current?.textContent ?? "",
+    });
+  };
   const renderSuggestion = (frame: FrameType) => {
-    // return frameRenderHandler({ frame: frame, address: "thought" });
     const thoughts = frame.thoughtFrames.map((tframe) =>
       ThoughtSchema.parse(JSON.parse(tframe.content ?? ""))
     );
     return thoughts.map((tframe, idx) => (
-      <div className="p-4 m-2 rounded-sm text-sm bg-green-200" key={idx}>
-        {tframe.sample_answer}
+      <div
+        className="p-1 rounded-sm text-sm flex flex-col gap-2 items-start relative bg-transparent"
+        key={idx}
+      >
+        <p className="border p-1 bg-green-600 text-sm rounded-sm text-white z-10">
+          Suggestion
+        </p>
+        <p ref={suggestionRef} className="text-sm bg-green-200 p-2 rounded-md">
+          {tframe.sample_answer}
+        </p>
+        <div className="flex w-full justify-end absolute bottom-0 right-0">
+          <Button
+            onClick={handleClick}
+            variant={"outline"}
+            size={"sm"}
+            className="p-1"
+          >
+            <CheckCircleIcon className="w-4 h-4" />
+          </Button>
+        </div>
       </div>
     ));
   };
