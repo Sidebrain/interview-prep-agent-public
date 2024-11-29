372d07d9e3f685b537afcc8a0860aa9d365846a6
Anudeep Yegireddi <anudeep.yegireddi@gmail.com>
2024-11-29 09:49:24 +0530

    bug: temporary fix for double frames by skipping duplicates in frame reducer

--- frontend/app/interview/reducers/frameReducer.ts
+++ frontend/app/interview/reducers/frameReducer.ts
@@ -1,3 +1,4 @@
+import clientLogger from "@/app/lib/clientLogger";
 import { WebsocketFrame } from "@/types/ScalableWebsocketTypes";
 
 export interface FrameState {
@@ -18,6 +19,15 @@ export const frameReducer = (
 ): FrameState => {
   switch (action.type) {
     case "ADD_FRAME":
+      // temporary check to prevent duplicate frames
+      if (
+        state.frames.find((frame) => frame.frameId === action.payload.frameId)
+      ) {
+        clientLogger.debug("Duplicate frame received", {
+          frameId: action.payload.frameId,
+        });
+        return state;
+      }
       return {
         ...state,
         frames: [...state.frames, action.payload],
