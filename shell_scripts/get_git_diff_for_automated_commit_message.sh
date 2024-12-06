02d7d11c747b3e28d41263cb49b34c78a138899f
Anudeep Yegireddi <anudeep.yegireddi@gmail.com>
2024-12-06 09:42:41 +0530

    chore: update diff shell script to not error when finding a deleted file

--- shell_scripts/get_git_diff_for_automated_commit_message.sh
+++ shell_scripts/get_git_diff_for_automated_commit_message.sh
@@ -5,6 +5,10 @@ git diff --cached >> commit_details.txt
 echo "### FILE CONTENTS ###" >> commit_details.txt
 for file in $(git diff --cached --name-only); do
   echo "### $file ###" >> commit_details.txt
-  cat "$file" >> commit_details.txt
+  if [ -f "$file" ]; then
+    cat "$file" >> commit_details.txt
+  else
+    echo "File deleted" >> commit_details.txt
+  fi
   echo "\n" >> commit_details.txt
 done
