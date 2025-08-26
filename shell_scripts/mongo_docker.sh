e0c67433284638f2ee2076b741408aac8c7c6b0b
anudeep22003 <anudeep.yegireddi@gmail.com>
2025-08-26 12:30:14 -0700

    chore: removed shell scripts and api key exposing files for making repo public

--- shell_scripts/mongo_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull mongodb/mongodb-community-server:latest
-docker run \
-  --name mongodb \
-  -p 27017:27017 \
-  -d \
-  -v mongodb_data:/data/db \
-  mongodb/mongodb-community-server:latest
